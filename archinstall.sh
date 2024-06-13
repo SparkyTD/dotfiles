d8ab73b5b93b337fe448d1d3d66a1c6f31f75314
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2024-06-13 22:44:47 +0300

    Added python-pywal to archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -12,7 +12,7 @@ LOCALE="en_US.UTF-8"
 PACKAGES_BASE=(base base-devel linux linux-firmware linux-headers sudo)
 PACKAGES_BOOT=(os-prober grub efibootmgr dosfstools mtools)
 PACKAGES_EXTRA=(git hyfetch htop curl wget mc nano neovim less github-cli networkmanager openssh tmux pavucontrol pipewire-pulse github-cli wl-clipboard openrgb fzf dotnet-sdk dotnet-runtime aspnet-runtime docker nmap openbsd-netcat)
-PACKAGES_GUI=(hyprland hyprpaper kitty alacritty waybar rofi-wayland ttf-firacode-nerd noto-fonts-emoji qt5ct nwg-look kvantum kvantum-qt5 dolphin xwaylandvideobridge xdg-desktop-portal xdg-desktop-portal-hyprland)
+PACKAGES_GUI=(hyprland hyprpaper kitty alacritty waybar rofi-wayland ttf-firacode-nerd noto-fonts-emoji qt5ct nwg-look kvantum kvantum-qt5 dolphin xwaylandvideobridge xdg-desktop-portal xdg-desktop-portal-hyprland python-pywal)
 PACKAGES_YAY=(google-chrome spotify discord github-desktop-bin jetbrains-toolbox postman-bin wdisplays grimshot imhex-bin)
 
 
